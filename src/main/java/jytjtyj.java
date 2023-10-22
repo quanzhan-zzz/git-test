1a4fef9eddec4db505e1064ceb5ae7f1c2ab6db1
zzz <2087309730@qq.com>
2023-10-22 11:52:52 +0800

    first

---
+++ src/main/java/jytjtyj.java
@@ -0,0 +1,14 @@
+/**
+ * jytjtyj
+ *
+ * @author zzz
+ * @create 2023/10/22 11:49
+ */
+
+public class jytjtyj {
+
+    public static void main(String[] args) {
+        System.out.println("bghfdjkghbfgd");
+    }
+
+}
